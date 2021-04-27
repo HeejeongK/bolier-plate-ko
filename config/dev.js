1759195ee0e5feba6d42c53852277b4e0de217e1
HeejeongK <susiet1992@gmail.com>
2021-04-27 13:31:50 +0900

    비밀정보 보호

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports ={
+    mongoURI: 'mongodb+srv://heejeong:1234567abcd@boilerplate.cdbqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
+//heroku 해로쿠..? 배포한 후엔 해로쿠에서 가져오게 해야함!~ 
--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+const config = require('./config/key')
+
+//application/x-www-form-rulencoded
+app.use(bodyParser.urlencoded({extended: true}));  
+
+//applicaion/json 분석해서 가져오게
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://heejeong:apfhd9294@boilerplate.cdbqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => res.send('너무 신기한 노드몬~~~?'))
+
+
+app.post('/register', (req, res)=> {
+  //회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+
+
+
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) =>{
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
